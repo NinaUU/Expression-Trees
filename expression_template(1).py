f7a4c5db8258975c343671d6751c907067ad41f6
isabeldroste <i.e.a.c.droste@uu.nl>
2017-03-27 17:31:37 +0200

    Update expression_template(1).py

--- expression_template(1).py
+++ expression_template(1).py
@@ -171,3 +171,5 @@ class AddNode(BinaryNode):
         super(AddNode, self).__init__(lhs, rhs, '+')
         
 # TODO: add more subclasses of Expression to represent operators, variables, functions, etc.
+
+#test
