acbed636c8362d97cf8deaf9de98150ae6555b8e
isabeldroste <i.e.a.c.droste@uu.nl>
2017-03-28 21:07:38 +0200

    Update expression_template(1).py
    Ik heb ervoor gezorgd dat er alleen haakjes geplaatst worden wanneer dit echt nodig is. Dit heb ik gedaan door een getal te verbinden aan de prioriteit van een operator. ** is prioriteit 1, (* en /) is prioriteit 2, (+ en -) is prioriteit 3. Wanneer er in de uitdrukking links of rechts van een operator een andere operator voorkomt met een lagere prioriteit (dus hoger getal), dan moeten er haakjes geplaatst worden, bijvoorbeeld (2 + 3) * 4.
    Dit werkt in de meeste gevallen, maar in sommige gevallen worden er nog onnodig haakjes geplaatst. Dit is het geval bij bijvoorbeeld:
    (2 + 3) * 4 * 5. Hier maakt hij dan ((2 + 3) * 4) * 5 van omdat er dan links van de tweede * een + voorkomt. Hier moet nog iets slims op bedacht worden.

--- expression_template(1).py
+++ expression_template(1).py
@@ -1,11 +1,12 @@
 import math
 
+
 # split a string into mathematical tokens
 # returns a list of numbers, operators, parantheses and commas
 # output will not contain spaces
 def tokenize(string):
     splitchars = list("+-*/(),")
-    
+
     # surround any splitchar by spaces
     tokenstring = []
     for c in string:
@@ -14,10 +15,10 @@ def tokenize(string):
         else:
             tokenstring.append(c)
     tokenstring = ''.join(tokenstring)
-    #split on spaces - this gives us our tokens
+    # split on spaces - this gives us our tokens
     tokens = tokenstring.split()
-    
-    #special casing for **:
+
+    # special casing for **:
     ans = []
     for t in tokens:
         if len(ans) > 0 and t == ans[-1] == '*':
@@ -25,7 +26,8 @@ def tokenize(string):
         else:
             ans.append(t)
     return ans
-    
+
+
 # check if a string represents a numeric value
 def isnumber(string):
     try:
@@ -34,7 +36,8 @@ def isnumber(string):
     except ValueError:
         return False
 
-# check if a string represents an integer value        
+
+# check if a string represents an integer value
 def isint(string):
     try:
         int(string)
@@ -42,37 +45,76 @@ def isint(string):
     except ValueError:
         return False
 
+# returns the number of priority of an operator
+# the lower the priority, the higher the number
+def op_nummer(op):
+    if op == '**':
+        return 1
+    if op == '*' or op == '/':
+        return 2
+    if op == '+' or op == '-':
+        return 3
+
+# returns priority number of the operator in the string with lowest priority
+# the lower the priority, the higher the number
+def zoek_op(st):
+    result = 1
+    for s in st:
+        if (s == '*' or s == '/') and result != 3:
+            result = 2
+        if s == '+' or s == '-':
+            result = 3
+    return result
+
+
 class Expression():
     """A mathematical expression, represented as an expression tree"""
-    
+
     """
     Any concrete subclass of Expression should have these methods:
      - __str__(): return a string representation of the Expression.
      - __eq__(other): tree-equality, check if other represents the same expression tree.
     """
+
     # TODO: when adding new methods that should be supported by all subclasses, add them to this list
-    
+
     # operator overloading:
     # this allows us to perform 'arithmetic' with expressions, and obtain another expression
     def __add__(self, other):
         return AddNode(self, other)
-        
+
+    def __sub__(self, other):
+        return SubNode(self, other)
+
+    def __mul__(self, other):
+        return MulNode(self, other)
+
+    def __truediv__(self, other):
+        return DivNode(self, other)
+
+    def __pow__(self, other):
+        return PowNode(self, other)
+
     # TODO: other overloads, such as __sub__, __mul__, etc.
-    
+
     # basic Shunting-yard algorithm
-    def fromString(string):
+    def fromString(string):  # van een string (infix) naar expressieboom
         # split into tokens
         tokens = tokenize(string)
-        
+
         # stack used by the Shunting-Yard algorithm
         stack = []
         # output of the algorithm: a list representing the formula in RPN
         # this will contain Constant's and '+'s
         output = []
-        
+
+        ## rang van operators
+        first_op_list = ['**']
+        second_op_list = ['*', '/']
+        third_op_list = ['+', '-']
         # list of operators
-        oplist = ['+']
-        
+        oplist = ['+', '-'] + second_op_list + first_op_list
+
         for token in tokens:
             if isnumber(token):
                 # numbers go directly to the output
@@ -83,10 +125,21 @@ class Expression():
             elif token in oplist:
                 # pop operators from the stack to the output until the top is no longer an operator
                 while True:
-                    # TODO: when there are more operators, the rules are more complicated
-                    # look up the shunting yard-algorithm
                     if len(stack) == 0 or stack[-1] not in oplist:
                         break
+                    if token in second_op_list and stack[-1] in third_op_list:
+                        break  ## dan moet hij op de stack
+                    if token in first_op_list and stack[-1] in second_op_list:
+                        break
+                    if token in first_op_list and stack[-1] in third_op_list:
+                        break  ##alle variaties van lagere rang
+                    if token in third_op_list and stack[-1] in third_op_list:
+                        break  ## machtsverheven in rechtsacciosatief, dus moeten achter elkaar op de stack
+
+                    # TODO: when there are more operators, the rules are more complicated
+                    # look up the shunting yard-algorithm
+                    ## werkt nu voor plus en min, allecombinaties
+
                     output.append(stack.pop())
                 # push the new operator onto the stack
                 stack.append(token)
@@ -103,74 +156,114 @@ class Expression():
             else:
                 # unknown token
                 raise ValueError('Unknown token: %s' % token)
-            
+
         # pop any tokens still on the stack to the output
         while len(stack) > 0:
             output.append(stack.pop())
-        
+
         # convert RPN to an actual expression tree
         for t in output:
             if t in oplist:
                 # let eval and operator overloading take care of figuring out what to do
                 y = stack.pop()
                 x = stack.pop()
-                stack.append(eval('x %s y' % t))
+                stack.append(eval('x %s y' % t))  ## combineer node met x en node met y met ouder node operator
             else:
                 # a constant, push it to the stack
                 stack.append(t)
         # the resulting expression tree is what's left on the stack
         return stack[0]
-    
+
+
 class Constant(Expression):
     """Represents a constant value"""
+
     def __init__(self, value):
         self.value = value
-        
+
     def __eq__(self, other):
         if isinstance(other, Constant):
             return self.value == other.value
         else:
             return False
-        
+
     def __str__(self):
         return str(self.value)
-        
+
     # allow conversion to numerical values
     def __int__(self):
         return int(self.value)
-        
+
     def __float__(self):
         return float(self.value)
-        
+
+
 class BinaryNode(Expression):
     """A node in the expression tree representing a binary operator."""
-    
+
     def __init__(self, lhs, rhs, op_symbol):
         self.lhs = lhs
         self.rhs = rhs
         self.op_symbol = op_symbol
-    
+
     # TODO: what other properties could you need? Precedence, associativity, identity, etc.
-            
+
     def __eq__(self, other):
         if type(self) == type(other):
             return self.lhs == other.lhs and self.rhs == other.rhs
         else:
             return False
-            
+
     def __str__(self):
         lstring = str(self.lhs)
         rstring = str(self.rhs)
-        
-        # TODO: do we always need parantheses?
-        return "(%s %s %s)" % (lstring, self.op_symbol, rstring)
-        
+
+        # Haakjes zijn alleen nodig wanneer er in de uitdrukking links of rechts van de operator een
+        # andere operator voorkomt die een lagere prioriteit heeft.
+        # Onderstaande code checkt of dit het geval is
+        if zoek_op(lstring) > op_nummer(self.op_symbol) and (zoek_op(rstring) <= op_nummer(self.op_symbol)):
+            return "(%s) %s %s" % (lstring, self.op_symbol, rstring)
+        if zoek_op(lstring) <= op_nummer(self.op_symbol) and zoek_op(rstring) > op_nummer(self.op_symbol):
+            return "%s %s (%s)" % (lstring, self.op_symbol, rstring)
+        if zoek_op(lstring) > op_nummer(self.op_symbol) and zoek_op(rstring) > op_nummer(self.op_symbol):
+            return "(%s) %s (%s)" % (lstring, self.op_symbol, rstring)
+        return "%s %s %s" % (lstring, self.op_symbol, rstring)
+
+
 class AddNode(BinaryNode):
     """Represents the addition operator"""
+
     def __init__(self, lhs, rhs):
         super(AddNode, self).__init__(lhs, rhs, '+')
-        
-# TODO: add more subclasses of Expression to represent operators, variables, functions, etc.
 
-#test
-#hallo
+
+class SubNode(BinaryNode):
+    """Represents the subtraction operator"""
+
+    def __init__(self, lhs, rhs):
+        super(SubNode, self).__init__(lhs, rhs, '-')
+
+
+class MulNode(BinaryNode):
+    """Represents the multiplication operator"""
+
+    def __init__(self, lhs, rhs):
+        super(MulNode, self).__init__(lhs, rhs, '*')
+
+
+class DivNode(BinaryNode):
+    """Represents the division operator"""
+
+    def __init__(self, lhs, rhs):
+        super(DivNode, self).__init__(lhs, rhs, '/')
+
+
+class PowNode(BinaryNode):
+    """Represents the power operator"""
+
+    def __init__(self, lhs, rhs):
+        super(PowNode, self).__init__(lhs, rhs, '**')
+        # TODO: add more subclasses of Expression to represent operators, variables, functions, etc.
+
+a = Expression.fromString('((2 + 3) * 4)')
+print(a)
